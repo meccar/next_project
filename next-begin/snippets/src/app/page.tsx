8e413caf5e2c646d692e1d351268ca508b3ec2ab
meccar <tung14hn@gmail.com>
2024-08-08 22:54:22 +0800

    updated

---
+++ next-begin/snippets/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+  return <div>Home page</div>;
+}
---
+++ next-begin/snippets/src/app/snippets/new/page.tsx
@@ -0,0 +1,52 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // This needs tobe a server action
+    "use server";
+
+    // CHeck the user's inputs and make sure they are valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // Create a new record in the db
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    // Redirect the user back to the root route
+    redirect("/");
+  }
+
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input name="title" className="border round p=2 w-full" id="title" />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
